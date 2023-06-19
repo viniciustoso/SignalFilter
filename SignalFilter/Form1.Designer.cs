7de0ad65ad39f862533e36ff0160f6a79e03bc0e
Foke <viniztoso@gmail.com>
2023-06-18 23:06:51 -0300

    Adicionar arquivos de projeto.

---
+++ SignalFilter/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace SignalFilter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.lowPassFilterField = new System.Windows.Forms.NumericUpDown();
+            this.lowPassFilterBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.highPassFilterBar = new System.Windows.Forms.TrackBar();
+            this.highPassFilterField = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.lowPassFilterField)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lowPassFilterBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.highPassFilterBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.highPassFilterField)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.zedGraphControl1.Location = new System.Drawing.Point(218, 27);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(369, 572);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // lowPassFilterField
+            // 
+            this.lowPassFilterField.Location = new System.Drawing.Point(69, 111);
+            this.lowPassFilterField.Name = "lowPassFilterField";
+            this.lowPassFilterField.Size = new System.Drawing.Size(53, 20);
+            this.lowPassFilterField.TabIndex = 1;
+            this.lowPassFilterField.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.lowPassFilterField.ValueChanged += new System.EventHandler(this.FilterField_ValueChanged);
+            // 
+            // lowPassFilterBar
+            // 
+            this.lowPassFilterBar.Location = new System.Drawing.Point(12, 137);
+            this.lowPassFilterBar.Maximum = 100;
+            this.lowPassFilterBar.Name = "lowPassFilterBar";
+            this.lowPassFilterBar.Size = new System.Drawing.Size(190, 45);
+            this.lowPassFilterBar.TabIndex = 2;
+            this.lowPassFilterBar.Scroll += new System.EventHandler(this.FilterBar_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Low pass filter cutoff";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Hz";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "High pass filter cutoff";
+            // 
+            // highPassFilterBar
+            // 
+            this.highPassFilterBar.Location = new System.Drawing.Point(12, 267);
+            this.highPassFilterBar.Maximum = 100;
+            this.highPassFilterBar.Name = "highPassFilterBar";
+            this.highPassFilterBar.Size = new System.Drawing.Size(190, 45);
+            this.highPassFilterBar.TabIndex = 6;
+            this.highPassFilterBar.Scroll += new System.EventHandler(this.FilterBar_Scroll);
+            // 
+            // highPassFilterField
+            // 
+            this.highPassFilterField.Location = new System.Drawing.Point(69, 241);
+            this.highPassFilterField.Name = "highPassFilterField";
+            this.highPassFilterField.Size = new System.Drawing.Size(53, 20);
+            this.highPassFilterField.TabIndex = 5;
+            this.highPassFilterField.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.highPassFilterField.ValueChanged += new System.EventHandler(this.FilterField_ValueChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(599, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.fileToolStripMenuItem.Text = "Arquivo";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.openToolStripMenuItem.Text = "Abrir...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.saveAsToolStripMenuItem.Text = "Salvar Como...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 611);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.highPassFilterBar);
+            this.Controls.Add(this.highPassFilterField);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lowPassFilterBar);
+            this.Controls.Add(this.lowPassFilterField);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Filtragem de sinal";
+            ((System.ComponentModel.ISupportInitialize)(this.lowPassFilterField)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lowPassFilterBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.highPassFilterBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.highPassFilterField)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.NumericUpDown lowPassFilterField;
+        private System.Windows.Forms.TrackBar lowPassFilterBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar highPassFilterBar;
+        private System.Windows.Forms.NumericUpDown highPassFilterField;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+    }
+}
+
---
+++ SignalFilter/Form1.cs
@@ -0,0 +1,272 @@
+﻿using Accord.Audio.Filters;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace SignalFilter
+{
+    public partial class Form1 : Form
+    {
+        private static List<double> SignalList = new List<double>
+        {
+            -0.00000047469604,
+            -0.000001306379,
+            0.0000055409564,
+            0.00003520742,
+            0.000052020758,
+            0.000060402552,
+            -0.000043129519,
+            -0.0001125016,
+            -0.00018260432,
+            -0.00015841764,
+            -0.00011617896,
+            -0.00013290522,
+            -0.00037435323,
+            -0.00068153447,
+            -0.00031832643,
+            0.00046329558,
+            -0.00037943243,
+            -0.00378769,
+            -0.00547472,
+            0.00042749097,
+            0.01129542,
+            0.01534481,
+            0.00580368,
+            -0.00856397,
+            -0.01388226,
+            -0.00761286,
+            0.00137434,
+            0.00595682,
+            0.00596679,
+            0.0031812,
+            -0.00125056,
+            -0.00466955,
+            -0.00398068,
+            -0.000065110777,
+            0.00263525,
+            0.00135817,
+            -0.00222557,
+            -0.00419043,
+            -0.00252298,
+            0.00150468,
+            0.00427969,
+            0.00348717,
+            0.00047537009,
+            -0.00111418,
+            -0.00012526485,
+            0.00067645544,
+            -0.00110193,
+            -0.00380943,
+            -0.00426841,
+            -0.00202688,
+            0.0010157,
+            0.00342666,
+            0.00453182,
+            0.00341483,
+            -0.000090311965,
+            -0.00399398,
+            -0.00504003,
+            -0.00176624,
+            0.00331933,
+            0.00576494,
+            0.00382156,
+            0.0001423753,
+            -0.00211393,
+            -0.00250719,
+            -0.00185713,
+            -0.00013720396,
+            0.00231429,
+            0.00343645,
+            0.00150276,
+            -0.00219182,
+            -0.00395536,
+            -0.00174808,
+            0.00208693,
+            0.00311306,
+            0.000041200336,
+            -0.00379334,
+            -0.00467363,
+            -0.00218674,
+            0.00148802,
+            0.00418122,
+            0.00460599,
+            0.00209465,
+            -0.00219807,
+            -0.00469068,
+            -0.00215968,
+            0.00351081,
+            0.00612001,
+            0.00310432,
+            -0.00089744007,
+            -0.00096810103,
+            0.00038296447,
+            -0.00236524,
+            -0.0079986,
+            -0.00873421,
+            -0.00056528149,
+            0.01055785,
+            0.01466104,
+            0.00731156,
+            -0.00676257,
+            -0.01739577,
+            -0.01596056,
+            -0.00286475,
+            0.01101428,
+            0.01418228,
+            0.00656275,
+            -0.00172098,
+            -0.00346449,
+            -0.00097349909,
+            0.00043476542,
+            -0.00024356153,
+            -0.00087278237,
+            -0.00062934903,
+            -0.00022400485,
+            -0.00018248211,
+            -0.00022873518,
+            -0.00019961091,
+            -0.00019236178,
+            -0.00014879073,
+            -0.000095523988,
+            -0.000012213283,
+            0.000010215717
+        };
+        private PointPairList OriginSeismic = new PointPairList();
+        private int SamplingRate = 4;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateOriginSignal();
+            InitializeGraph();
+            SetDefaultValues();
+        }
+
+        private void CreateOriginSignal()
+        {
+            int ms = -SamplingRate;
+
+            foreach (double signal in SignalList)
+                OriginSeismic.Add(signal, ms += SamplingRate);
+        }
+
+        private void InitializeGraph()
+        {
+            GraphPane graphPane = zedGraphControl1.GraphPane;
+            graphPane.CurveList.Clear();
+
+            graphPane.Title.IsVisible = false;
+            graphPane.XAxis.Title.Text = "Amplitude";
+            graphPane.YAxis.Title.Text = "Tempo (ms)";
+
+            graphPane.AddCurve("Sísmica Original", OriginSeismic, Color.Black, SymbolType.None);
+
+            graphPane.Fill = new Fill(Form1.DefaultBackColor);
+            graphPane.Border = new Border(false, Color.Empty, 0);
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void SetDefaultValues()
+        {
+            lowPassFilterBar.Maximum = (int)lowPassFilterField.Maximum;
+        }
+
+        private void GenerateGraph(PointPairList filteredSeismic)
+        {
+            GraphPane graphPane = zedGraphControl1.GraphPane;
+
+            graphPane.CurveList.Clear();
+
+            graphPane.AddCurve("Sísmica Filtrada", filteredSeismic, Color.Blue, SymbolType.None);
+            graphPane.AddCurve("Sísmica Original", OriginSeismic, Color.Black, SymbolType.None);
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private PointPairList FilterSeismic(NumericUpDown field)
+        {
+            PointPairList filteredSeismic = new PointPairList();
+            int ms = -SamplingRate;
+
+            foreach (double signal in SignalList)
+            {
+                double filteredSignal = field.Name == "lowPassFilterField" ?
+                    LowPassFilter.GetAlpha(signal, Convert.ToDouble(field.Value)) :
+                    HighPassFilter.GetAlpha(Convert.ToDouble(field.Value), signal);
+
+                filteredSeismic.Add(filteredSignal, ms += SamplingRate);
+            }
+
+            return filteredSeismic;
+        }
+
+        private void FilterField_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown field = (NumericUpDown)sender;
+
+            GenerateGraph(FilterSeismic(field));
+
+            if (field.Name == "lowPassFilterField")
+                lowPassFilterBar.Value = (int)field.Value;
+            else
+                highPassFilterBar.Value = (int)field.Value;
+        }
+
+        private void FilterBar_Scroll(object sender, EventArgs e)
+        {
+            TrackBar field = (TrackBar)sender;
+
+            if (field.Name == "lowPassFilterBar")
+                lowPassFilterField.Value = field.Value;
+            else
+                highPassFilterField.Value = field.Value;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (Path.GetExtension(openFileDialog1.FileName) != ".txt")
+                        throw new IOException("Não foi possível abrir a configuração de filtros.");
+
+                    string[] lines = File.ReadAllLines(openFileDialog1.FileName);
+
+                    lowPassFilterField.Value = Convert.ToDecimal(lines[0]);
+                    highPassFilterField.Value = Convert.ToDecimal(lines[1]);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (Path.GetExtension(saveFileDialog1.FileName) != ".txt")
+                        throw new IOException("Não foi possível salvar a configuração de filtros.");
+
+                    string filters = lowPassFilterField.Value + Environment.NewLine + highPassFilterField.Value;
+
+                    File.WriteAllText(saveFileDialog1.FileName, filters);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
